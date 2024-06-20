93dfa4fb12c5d382130ef53a2a820d375a776c61
Guga <gugizm@gmail.com>
2024-06-20 17:23:11 +0400

    clear project

--- idea_incubator/rooms/models.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-from django.db import models
-from posts.models import Post
-
-
-class Room(models.Model):
-    post = models.OneToOneField(Post, on_delete=models.CASCADE)
